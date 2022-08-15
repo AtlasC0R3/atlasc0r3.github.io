e4447e1c2a92198a0a049466786e7b4d0dee55bb
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-15 17:18:58 -0400

    bootstrap studio export

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,7 +2,7 @@ var backgrounds = [
     {
         "name": "The Presence",
         "description": "Reference to Nine Inch Nails' 2007 release/ARG, Year Zero. The Presence is involved in this as a hand that reaches down from the sky. 'Some say it was a warning, some say it was a sign.'",
-        "bg_url": "themes/presence.png",
+        "bg_url": "assets/img/themes/presence.png",
         "bg_size": "cover",  // cover fits image to fit on screen without borders/stretching, contain puts image as big as possible while not losing any of the image but causes black bars, auto just kinda puts it there
         "bg_blend": 1,  // darken background image (or, really, enable use of "background-blend-mode: difference" in CSS)
         "text_color": 0,  // 0: dark, 1: white
@@ -12,7 +12,7 @@ var backgrounds = [
     {
         "name": "Bliss",
         "description": "Who didn't like Windows XP?",
-        "bg_url": "themes/bliss.jpg",
+        "bg_url": "assets/img/themes/bliss.jpg",
         "bg_blend": 1,  // darken background image (or, really, enable use of "background-blend-mode: difference" in CSS)
         "text_color": 1,  // 0: dark, 1: white
         "text_shadow": 1,  // 1: activate, 0: disable
