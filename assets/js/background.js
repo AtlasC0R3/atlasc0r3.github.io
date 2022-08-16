2816852d0fa0d7fca1ae8574a97748fa7092a2d2
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-16 00:16:46 -0400

    bootstrap studio export

--- assets/js/background.js
+++ assets/js/background.js
@@ -8,7 +8,8 @@ var backgrounds = [
         "text_color": 0,  // 0: dark, 1: white
         "text_shadow": 1,  // 1: activate, 0: disable
         "footer_color": 2,  // 0: inherit (disable), 1: dark, 2: white
-        "font": "Consolas"
+        "font": "Consolas",
+        "position": "right top"  // align to where? left, center, right, bottom
     },
     {
         "name": "Bliss",
@@ -19,17 +20,40 @@ var backgrounds = [
         "text_shadow": 1,  // 1: activate, 0: disable
         "bg_size": "cover",  // cover fits image to fit on screen without borders/stretching, contain puts image as big as possible while not losing any of the image but causes black bars, auto just kinda puts it there
         "font": "Tahoma"
+    },
+    {
+        "name": "Pukebucket",
+        "description": "What the fuck. Blech.",
+        "bg_url": "assets/img/themes/trash.jpg",
+        "bg_blend": 1,  // darken background image (or, really, enable use of "background-blend-mode: difference" in CSS)
+        "text_color": 1,  // 0: dark, 1: white
+        "text_shadow": 1,  // 1: activate, 0: disable
+        "bg_size": "contain",  // cover fits image to fit on screen without borders/stretching, contain puts image as big as possible while not losing any of the image but causes black bars, auto just kinda puts it there
+        "font": "Comic Sans MS"
+    },
+    {
+        "name": "Win95",
+        "description": "Even if it came out in 1995, you still gotta admit we wouldn't be using computers if it weren't for that OS.",
+        "bg_url": "assets/img/themes/win95.jpg",
+        "bg_blend": 0,
+        "text_color": 1,
+        "text_shadow": 0,
+        "bg_size": "cover",
+        "font": "sans-serif",
+        "position": "left bottom"
     }
 ]
-
-// TODO: footer color
 
 function set_background(element){
     // Basic CSS stuff
     document.body.style.backgroundImage = `url('${element.bg_url}')`;
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundAttachment = "fixed";  // CAUSES GRAPHICAl GLITCHES ON MOBILE
-    document.body.style.backgroundPosition = "center";
+
+    var position = "center";
+    if(element.position) position = element.position;
+    document.body.style.backgroundPosition = position;
+
     document.body.style.backgroundSize = element.bg_size;
     if(!element.text_color) document.body.style.color = "var(--bs-dark)";  // set text dark
     if(element.text_shadow) document.body.style.textShadow = "0px 0px 12px #000000";
