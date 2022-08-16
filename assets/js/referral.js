7dc3dc3ad92280a53a725079b9eedc9351859f19
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-16 18:59:39 -0400

    bootstrap studio export

--- assets/js/referral.js
+++ assets/js/referral.js
@@ -7,8 +7,8 @@ function check_fediring() {
         "tomxcd.me"]
         .includes(document.referrer
             .replace("https://", '')
-            .replace("http://", ''))
-            .split('/', 1)[0])
+            .replace("http://", '')
+            .split('/', 1)[0]))
             console.log("Seems like you came here from Fediring, no?");
     // the array is due to the way webrings work; websites are required to have "previous" and "next" links.
     // I was gonna think that the referral string would just be fediring.net, but unfortunately, no.
@@ -16,3 +16,5 @@ function check_fediring() {
     // Which is fine, it's a hacky solution, but it works. Except for the /random strategy.
     // TODO: The idea I have for this is to have a floating menu with the fediring links (or a block being shown early in the page), with a message that says "Hey, looks like you came here from Fediring! Here's some links for easier exploration:"
 }
+
+check_fediring();
