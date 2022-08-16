e9433faaaefc91f49fe13d22dd622c00a53b8f9f
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-15 20:03:52 -0400

    changed website title

---
+++ assets/js/title.js
@@ -0,0 +1,15 @@
+var titles = [
+    "hi",
+    "certified human being",
+    "a Ubisoft original",
+    "made in Canada",
+    "vidja gaemr",
+    "with ADHD",
+    "thing doer",
+    "recommended by 9 out of 10 dentists",
+    "as seen on TV",
+    "the human being ever",
+    "stupid fuck"
+]
+
+document.title = "atlas_core, " + titles[Math.floor(Math.random()*titles.length)]
