569afdd2d00b17b07d1377b1eb9105a3b8521abe
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-15 23:32:54 -0400

    added more tab titles

--- assets/js/title.js
+++ assets/js/title.js
@@ -10,7 +10,16 @@ var titles = [
     "as seen on TV",
     "the human being ever",
     "stupid fuck",
-    "killed a spider"
+    "killed a spider",
+    "New Funky Mode",
+    "dear god anything but a waifu",
+    "with illusions of enlightenment",
+    "person who watched all Matrix movies",
+    "un épais québécois qui a rien à faire",
+    "music enjoyer",
+    "Spy x Family is decent",
+    "the only person that watches anime right after a gory show made in the United States by very American people in a very American plot",
+    "privacy enjoyer"
 ]
 
 document.title = "atlas_core, " + titles[Math.floor(Math.random()*titles.length)]
