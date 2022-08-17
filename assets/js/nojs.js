f45388912b173e4c6667a0419d0d8361bf200e7f
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2022-08-16 20:56:41 -0400

    bootstrap studio export

--- assets/js/nojs.js
+++ assets/js/nojs.js
@@ -2,3 +2,5 @@ function hideScriptDisabled() {
     document.getElementById("jsDisabled").hidden = true; // Bootstrap seems to function this way.
 }
 hideScriptDisabled();
+
+document.getElementById("midibutton").style.display = "block";
