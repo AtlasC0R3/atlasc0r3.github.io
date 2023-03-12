3597b035c1e7b5d6de14d07231209e08784653c7
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2023-03-12 18:10:12 -0400

    this should work now

--- assets/js/adblocker.js
+++ assets/js/adblocker.js
@@ -7,4 +7,10 @@
 // Note: this script was named "popunder.js", but then uBlock Origin stopped blocking it. Sad.
 // So I renamed it to either utep_ad.js or yzfdmoan.js. I picked the former because the latter had a certain word...
 
-document.getElementById("fuckfuckshitgoddammitfuckingcuntshit").style.display = "none"
+     
+if(document.querySelector(".ad-zone").offsetHeight){
+    console.log("Okay, so you have the reflex of checking your browser's console output, \
+    but not one to get an ad blocker? What's wrong with you?!")
+} else{
+    document.getElementById("fuckfuckshitgoddammitfuckingcuntshit").style.display = "inline"
+}
