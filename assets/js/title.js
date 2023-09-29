e369ecf80f3c1181050640d1b1b4e5b25c7bcea4
Atlas <atlas@azuki>
2023-09-28 20:39:17 -0400

    Revert "Revert "updated some text and why the fuck does it still want to "change" every single goddamn file""
    This reverts commit 581ec375d95364a94c3d174dacb9177d4d9a1072.

--- assets/js/title.js
+++ assets/js/title.js
@@ -4,7 +4,6 @@ var titles = [
     "a Ubisoft original",
     "made in Canada",
     "vidja gaemr",
-    "with ADHD",
     "thing doer",
     "recommended by 9 out of 10 dentists",
     "the human being ever",
@@ -20,7 +19,12 @@ var titles = [
     "pride of ${subject.hometown}",
     "Mug Moment",
     "batteries not included",
-    "2+2 equals 10 (IN BASE FOUR! I'M FINE!)"  // it's okay, I barely even had math classes during 9th grade. not to mention that was a Portal reference.
+    "2+2 equals 10 (IN BASE FOUR! I'M FINE!)",  // it's okay, I barely even had math classes during 9th grade. not to mention that was a Portal reference.
+    "Wii U owner",
+    "nerd",
+    "Halo disliker",
+    "free time enjoyer",
+    "gluten free"
 ]
 
 document.title = "atlas_core, " + titles[Math.floor(Math.random()*titles.length)]
