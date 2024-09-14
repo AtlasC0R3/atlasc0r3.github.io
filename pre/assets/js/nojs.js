9c6c3d32c681d7309f68c6ac0235b9bc2f57f45f
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2024-09-14 13:05:03 +0000

    I don't think anyone will miss the old versions

--- pre/assets/js/nojs.js
+++
@@ -1,7 +0,0 @@
-function hideScriptDisabled() {
-    console.log("JavaScript is enabled");
-    document.getElementById("jsDisabled").remove();  // Destructive, but it works. So I don't care.
-
-    document.getElementById("fuckfuckshitgoddammitfuckingcuntshit").style.display = "inline"
-}
-hideScriptDisabled();
