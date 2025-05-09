5acce80776849fea6980c8fe339f9242228872b7
atlas_core <39468657+AtlasC0R3@users.noreply.github.com>
2025-05-09 16:08:52 -0400

    pettiness is likely not a great idea

--- assets/js/title.js
+++ assets/js/title.js
@@ -27,22 +27,4 @@ var titles = [
     "gluten free"
 ]
 
-var pron = [
-    "doesn't/matter",
-    "who gives a fuck",
-    "eh",
-    "want my zodiac signs instead?",
-    "that's all you care about?",
-    "irrelevant",
-    "are an excuse for people to despise one another",
-    "don't be stupid",
-    "sequels to The Matrix weren't good",
-    "<b>sod off.</b>",
-    "ahem",
-    "edgy joke here",
-    "what the fuck is that"
-]
-
 document.title = "atlas_core, " + titles[Math.floor(Math.random()*titles.length)]
-
-document.getElementById("pronoun").innerHTML = "pronouns: <i>" + pron[Math.floor(Math.random()*pron.length)] + "</i>"
